7c4d069798727bb1f9aef29bdce541bad01ae332
justsj11 <sagnikjanacodes@gmail.com>
2021-12-06 17:57:05 +0530

    Initial commit

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,33 @@
 import React from 'react'
 
-function navbar() {
+
+function Navbar(props) {
     return (
-        <h1>I am the navbar</h1>
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">{props.title}</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/">{props.aboutTheProduct}</a>
+                            </li>
+                        </ul>
+                        <form className="d-flex">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </div>
     )
 }
 
-export default navbar
+export default Navbar
