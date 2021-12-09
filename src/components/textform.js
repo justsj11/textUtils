c87a86e6b16273e145da594e1701bee5596cc997
justsj11 <sagnikjanacodes@gmail.com>
2021-12-09 08:42:19 +0530

    COmpleted my very first react app

--- src/components/textform.js
+++ src/components/textform.js
@@ -1,29 +1,45 @@
-import React, { useState } from 'react'
-//import { text } from 'stream/consumers';
 
 
-function Textform(props) {
-    
+import React, {useState} from 'react'
+
+
+export default function TextForm(props) {
     const handleUpClick = ()=>{
-        console.log ( "Uppercase was clicked");
+        // console.log("Uppercase was clicked: " +  text);
         let newText = text.toUpperCase();
-        SetText("newtext")
-        }
-        const handleOnChange = (event)=>{
-            console.log("On change");
-            setText(event.target.value);
-            }
+        setText(newText)
+    }
+    const handleLoClick = ()=>{
+        // console.log("Uppercase was clicked: " +  text);
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+
+    const handleOnChange = (event)=>{
+        // console.log("On change");
+        setText(event.target.value)
+    }
+
+    const [text, setText] = useState(''); 
+    // text = "new text"; // Wrong way to change the state
+    // setText("new text"); // Correct way to change the state
     return (
-        <form>
-            <div className="form-group">
-                <h1>{props.heading}</h1>
-                <textarea className="form-control container" id="exampleFormControlTextarea1" rows="8"></textarea>
-                <button className="btn btn-primary" onClick = {handleUpClick()} onChange={handleOnChange} >Convert to UpperCase</button>
+        <>
+        <div className="container"> 
+            <h1>{props.heading}</h1>
+            <div className="mb-3"> 
+            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-        </form>
+            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+        </div>
+        <div className="container my-3">
+            <h2>Your text summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 *  text.split(" ").length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
-export default Textform;
-
-
-
